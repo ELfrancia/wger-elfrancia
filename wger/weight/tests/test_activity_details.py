43e74c6d532d1ac46301fb9c15a5bab043d9e489
Francesco Adreani <francesco.adreani@gmail.com>
2026-07-17 00:44:23 +0200

    test(weight): fix self.user reference in activity details test

---
+++ wger/weight/tests/test_activity_details.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from django.urls import reverse
+from django.utils import timezone
+from django.contrib.auth.models import User
+from wger.core.tests.base_testcase import WgerTestCase
+from wger.core.models import DailyActivity
+
+class ActivityDetailsTestCase(WgerTestCase):
+    """
+    Test case per l'endpoint dei dettagli dell'attività giornaliera
+    """
+
+    def setUp(self):
+        super().setUp()
+        self.user_login('member1')
+        self.user = User.objects.get(username='member1')
+
+    def test_get_activity_details_default(self):
+        """Testa la GET senza parametri (data odierna)"""
+        response = self.client.get(reverse('weight:activity-details'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Activity Calendar')
+
+    def test_get_activity_details_specific_date(self):
+        """Testa la GET con una data specifica"""
+        selected_date = '2026-07-16'
+        response = self.client.get(reverse('weight:activity-details'), {'date': selected_date})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, selected_date)
+
+    def test_post_log_activity_steps(self):
+        """Testa la POST per loggare i passi su una data specifica"""
+        selected_date = '2026-07-16'
+        # Log iniziale
+        response = self.client.post(
+            reverse('weight:activity-details'),
+            {
+                'date': selected_date,
+                'activity_type': 'steps',
+                'amount': '1000'
+            }
+        )
+        self.assertEqual(response.status_code, 200)
+        activity = DailyActivity.objects.get(user=self.user, date=selected_date)
+        self.assertEqual(activity.steps, 1000)
